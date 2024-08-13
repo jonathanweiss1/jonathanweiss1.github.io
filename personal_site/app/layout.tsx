e3d65d619f4a562625f84b21a5a26bcd3b5828cb
Jonathan Weiß <jonathan.weiss.ansbach@gmail.com>
2024-08-13 16:19:19 +0200

    Add google tag manager

--- personal_site/app/layout.tsx
+++ personal_site/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/layout";
+import { CookieBanner } from "@/components/cookieBanner"
+import { GoogleTagManager } from "@next/third-parties/google"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId={`${process.env.GTM_ID}`} />
       <body className={inter.className}>
+        <CookieBanner />
         <Layout>
           {children}
         </Layout>
