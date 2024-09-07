64468bc0c18afdab7fb7357e9f3d7b97ef9c047b
Jonathan Weiß <jonathan.weiss.ansbach@gmail.com>
2024-09-07 19:21:46 +0200

    add image rendering and some styling for articles overview

--- personal_site/components/technicalNotesPreview.tsx
+++ personal_site/components/technicalNotesPreview.tsx
@@ -9,11 +9,11 @@ export function TechnicalNotesPreview({ slug, title, text, image }: { slug: stri
     const currentRoute = usePathname();
     const currentLocale = getCurrentLocale(currentRoute);
     const readMore = currentLocale === 'en' ? 'Read more' : 'Weiterlesen';
-    return <div className="flex flex-row bg-background m-4 p-4 rounded gap-3">
-        <div className="w-1/5">
+    return <div className="flex flex-col md:flex-row bg-background m-4 p-4 rounded gap-3">
+        <div className="w-full md:w-1/5">
         <Image className="rounded object-contain" sizes="10vw" src={image.url} alt={image.description} width={image.width} height={image.height} />
         </div>
-        <div className="flex flex-col w-4/5">
+        <div className="flex flex-col w-full md:w-4/5">
         <span className="font-bold text-lg inline-block align-top underline">{title}</span>
         <div>{text}</div>
         <Link className="text-accent" href={getLinkToLocale(`/technical-notes/${slug}`, currentLocale, currentLocale)}>{readMore}</Link>
