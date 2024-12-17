a23cc414dd82a23116047fc9ce93453b7e357b3b
Jonathan Weiß <jonathan.weiss.ansbach@gmail.com>
2024-12-17 04:19:36 +0100

    Add under construction notice to homepage

--- personal_site/app/page.tsx
+++ personal_site/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="mt-20 md:w-2/3 w-5/6 text-primary bg-white rounded md:p-16 p-10 grid grid-cols-1 md:grid-cols-3">
       <div className="text-9xl md:order-last mb-16 md:mb-0 text-center">🚧</div>
       <div className="col-span-2 flex-col">
-        <h1 className="text-5xl font-bold">Hi! My website is currently under construction.</h1>
+        <h1 className="md:text-5xl text-2xl font-bold">Hi! My website is currently under construction.</h1>
         <h2 className="text-lg mt-2">Contact me here: jonathanweiss@posteo.de</h2>
       </div>
       </div>
